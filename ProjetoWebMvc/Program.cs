a9360bae156300ea2d17703ff2f723342fb3f362
Mateus Felipe Ribeiro <nogueira.mateus20k1@outlook.com>
2024-05-13 15:59:33 -0300

    att1

--- ProjetoWebMvc/Program.cs
+++ ProjetoWebMvc/Program.cs
@@ -20,5 +20,7 @@ namespace ProjetoWebMvc
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        <p>oi<p>
     }
 }
